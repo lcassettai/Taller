0b78c9a437bf9903a2f059c78ca597c658267d12
Luciano Cassettai <lcassettai>
2022-10-15 00:38:50 -0300

    Add conexion a la BD

---
+++ src/main/java/DataBase/Conexion.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package DataBase;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author win
+ */
+public class Conexion {
+    public static Connection conectar() {
+		Connection con = null;
+		
+		String password = "";
+		String usuario = "root";
+		String url = "jdbc:mysql://localhost:3306/mecanica?user=" + usuario
+				+ "&password=" + password;
+		try {
+			con = DriverManager.getConnection(url);
+			if (con != null) {
+				System.out.println("Conectado");
+			}
+		} catch (SQLException e) {
+			System.out.println("No se pudo conectar a la base de datos");
+			e.printStackTrace();
+		}
+		return con;
+	}
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module Controlador {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.base;
+    requires java.sql;
 
     opens Controlador to javafx.fxml;
     exports Controlador;
